d87987337d96a1524ce4493b50fae0b50e5a0ec0
Jong-eun-Lee <kjayjay40@gmail.com>
2021-02-08 22:27:24 +0900

    2021-02-08

--- 자료구조/l_AVLtree.py
+++ 자료구조/l_AVLtree.py
@@ -20,68 +20,21 @@
 #            A   z
 #               B  C
 # z를 기준으로 right rotation 하면 (right rotation at z)
-# z는 오른쪽으로 내려가고 x가 위쪽으로. B은 x가 아닌 z에 붙음.
+# 위와 같이 z는 오른쪽으로 내려가고 x가 위쪽으로. B은 x가 아닌 z에 붙음.
 # x와 A는 올라가면서 한 레벨 줄어듦
 # z와 C는 내려가면서 한 레벨 늘어남.
 # B는 똑같은 레벨.
-
-# x는 B 버리고 z를 자식으로 삼고 z는 B를 새로운 자식으로 삼는다.
-# x와 z.parent와의 관계, x와 z와의 관계, z와 B와의 관계 갱신
 # z.parent.child = x
 # x.parent = z.parent
 # x.right = z
 # z.parent = x
 # z.left = B
 # B.parent = z
-# 이것을 다시 left rotation 해주면 원래 모양대로 돌아간다.
 
-def rotateRight(self, z):
-    if z == None: return
-    x = z.left
-    if x == None: return
-    b = x.right
-
-    x.parent = z.parent
-    if z.parent:
-        if z.parent.left == z:
-            z.parent.left = x
-        else:
-            z.parent.right = x
-    x.right = z
-    z.parent = x
-    z.left = b
-    if b:
-        b.parent = z
-    if z == self.root:
-        self.root = x
-    # height에 대한 정보를 가지는 클래스를 이용한다면
-    # x와 z의 height 값을 update 해줘야 한다.
-
-def rotateLeft(self, z):
-    if z == None: return
-    x = z.right
-    if x == None: return
-    b = x.left
-
-    x.parent = z.parent
-    if z.parent:
-        if z.parent.left == z:
-            z.parent.left = x
-        else:
-            z.parent.right = x
-        
-    x.left = z
-    z.parent = x
-        
-    z.right = b
-    if b: b.parent = z
-
-    if z == self.root:
-        self.root = z
 #===========================================================================================#
 
 ## AVL Tree (Adelson-Velsky & Landis, 1962)
-# 모든 노드에 대해서 노드의 왼쪽 부트리와 오른쪽 부트리의 높이 차가 1이하인 BST
+# 모든 노드에 대해서 노드의 왼쪽 부트리와 오른쪽 부트리의 높이 차가 1이하인 BST.
 # 그러면 이 조건을 만족하면 항상 높이가 O(log n)이 되는가?
 # 증명(높이가 h이고 노드 개수가 n인 AVL tree가 h <= c*(log2 n) 임을):
 # h = 0 최소 노드 수 1
@@ -116,32 +69,191 @@ def rotateLeft(self, z):
 
 
 class Node:
-    def __init__(self, key, parent=None, left=None, right=None):
+    def __init__(self, key=None, parent=None, left=None, right=None):
         self.key = key
         self.parent = parent
         self.left = left
         self.right = right
-        self.height = -1
-        # 존재하지 않는 노드의 높이를 -1로 할 경우
-        # 자식 노드가 없는 노드의 높이를 max(-1, -1) + 1. 즉 0으로 계산할 수 있어 make sense.
+        self.height = 0
     
     def __str__(self):
         return str(self.key)
 
-# AVL 조건: abs(leftHeight - rightHeight) <= 1
-class AVLtree:
+    def __iter__(self): # inorder 방식으로
+        if self:
+            if self.left:
+                for e in self.left:
+                    yield e
+            yield self.key
+            if self.right:
+                for e in self.right:
+                    yield e
+    
+    def rightHeight(self):
+        if self.right:
+            return self.right.height
+        return -1 # 존재하지 않는 노드의 높이는 -1로 함.
+        # 자식이 없는 노드의 높이를 max(-1, -1) + 1
+        # 즉 0으로 계산할 수 있어 make sense.
+    
+    def leftHeight(self):
+        if self.left:
+            return self.left.height
+        return -1
+
+class BST:
     def __init__(self):
         self.root = None
         self.size = 0
 
-    def __len__(self):
-        return self.size
+    def __iter__(self):
+        return self.root.__iter__()
+    
+    def __str__(self):
+        return " - ".join(str(key) for key in self)
+    
+    def __contains__(self, key): # key가 존재한다면 True, 그렇지 않으면 False 반환
+        return self.search(key) != None
+    
+    def preorder(self, v):
+        if v:
+            print(v, end=" ")
+            self.preorder(v.left)
+            self.preorder(v.right)
+        
+    def inorder(self, v):
+        if v:
+            self.inorder(v.left)
+            print(v, end=" ")
+            self.inorder(v.right)
+    
+    def postorder(self, v):
+        if v:
+            self.postorder(v.left)
+            self.postorder(v.right)
+            print(v, end=" ")
+    
+    def search(self, key):
+        p = self.findLocation(key)
+        if p and p.key == key:
+            return p
+        else: return None
+    
+    def findLocation(self, key):
+        if self.size == 0:
+            return None
+        p = None
+        v = self.root
+        while v:
+            if v.key == key:
+                return v
+            else:
+                if v.key < key:
+                    p = v
+                    v = v.right
+                else:
+                    p = v
+                    v = v.left
+        return p # 매칭되는 key가 없다면 key가 들어갈 수 있는 자리의 부모 노드를 return
 
-    def height(self, v):
-        if v == None:
+    def insert(self, key):
+        p = self.findLocation(key)
+        if p == None or p.key != key:
+            v = Node(key)
+            if p == None:
+                self.root = v
+            else:
+                v.parent = p
+                if p.key < key:
+                    p.right = v
+                else:
+                    p.left = v
+            self.size += 1
+            self.updateHeight(v)
+            return v
+        else:
+            print("The key is already in tree.")
+            return p
+    
+    def deleteByCopying(self, x):
+        if x == None:
+            return None
+        a = x.left
+        if a == None:
+            b, p = x.right, x.parent
+            if p == None:
+                self.root = b
+            else:
+                if p.left == x:
+                    p.left = b
+                else:
+                    p.right = b
+            if b:
+                b.parent = p
+            del x
+        else:
+            m = a # m은 a 부트리 중 가장 큰 것
+            while m.right:
+                m = m.right
+            x.key = m.key
+            l, p = m.left, m.parent
+            if p.left == m:
+                p.left = l
+            else:
+                p.right = l
+            if l:
+                l.parent = p
+            del m
+        self.size -= 1
+        self.updateHeight(p)
+        return p # x를 채우기 위해 빠져나간 m의 본래 부모 노드를 return.
+    
+    def deleteByMerging(self, x):
+        if x == None:
+            return None
+        a, b, p = x.left, x.right, x.parent
+        if a == None:
+            replace = b
+            s = p
+        else:
+            replace = m = a # replace와 m은 일단 a로
+            while m.right:
+                m = m.right
+            m.right = b
+            if b:
+                b.parent = m
+            s = m
+        if self.root == x:
+            if replace:
+                replace.parent = None
+            self.root = replace
+        else:
+            if p.left == x:
+                p.left = replace
+            else:
+                p.right = repalce
+            if replace:
+                replace.parent = p
+        del x
+        self.size -= 1
+        self.updateHeight(s)
+        return s
+    
+    def height(self, x):
+        if x == None:
             return -1
-        return v.height
-
+        return x.height
+    
+    def updateHeight(self, v):
+        while v != None:
+            l, r = -1, -1
+            if v.left:
+                l = v.left.height
+            if v.right:
+                r = v.right.height
+            v.height = max(l, r) + 1
+            v = v.parent
+    
     #  z.p     =>    z.p
     #   z      =>     x
     # a   x    =>   z   c
@@ -155,17 +267,16 @@ class AVLtree:
         if z.parent:
             if z.parent.left == z:
                 z.parent.left = x
-            else:
+            if z.parent.right == z:
                 z.parent.right = x
         x.left = z
         z.parent = x
         z.right = b
-        if b: b.parent = z
-        if z == self.root:
-            self.root = z
-        z.height = max(self.height(z.left), self.height(z.right)) + 1
-        x.height = max(self.height(x.left), self.height(x.right)) + 1
-        return x # z 자리에 올라온 x return
+        if b:
+            b.parent = z
+        if self.root == z:
+            self.root = x
+        self.updateHeight(z)
 
     def rotateRight(self, z):
         if z == None: return
@@ -176,25 +287,28 @@ class AVLtree:
         if z.parent:
             if z.parent.left == z:
                 z.parent.left = x
-            else:
+            if z.parent.right == z:
                 z.parent.right = x
         x.right = z
         z.parent = x
         z.left = b
         if b:
             b.parent = z
-        if z == self.root:
+        if self.root == z:
             self.root = x
-        z.height = max(self.height(z.left), self.height(z.right)) + 1
-        x.height = max(self.height(x.left), self.height(x.right)) + 1
-        return x
+        self.updateHeight(z)
 
-    def bF(self, v): # Balance Factor
-        return self.height(v.left) - self.height(v.right)
-        
+class AVLtree(BST):
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+    
     def isBalanced(self, v):
-        return abs(self.bF(v)) <= 1
-
+        return abs(v.leftHeight() - v.rightHeight()) <= 1
+    
     # rebalance case는 총 네 가지(z가 unbalanced 하다고 하자).
     # right-right case solved by "rotateLeft at z"
     #     z            ->       y
@@ -208,146 +322,60 @@ class AVLtree:
     #       x  T4  ->         T2    y      -> T1 T2  T3 T4
     #     T2 T3    ->             T3  T4   ->
     # (left-right case는 rotateLeft at y 후 rotateRight at z로 해결)
-    def rebalance(self, z):
-        if self.bF(z) < -1: # z의 오른쪽 부트리 높이가 큰 불균형
-            if self.bF(z.right) > 0: # z 불균형의 right-left case
-                z.right = self.rotateRight(z.right)
-            w = self.rotateLeft(z)
-            return w
-        elif self.bF(z) > 1:
-            if self.bF(z.left) < 0: # left-right case
-                z.left = self.rotateLeft(z.left)
-            w = self.rotateRight(z)
-            return w
-        else: return
-
-    def findLocation(self, key):
-        if self.size == 0:
-            return None
-        v= self.root
-        p = None
-        while v != None:
-            if v.key == key:
-                return v
-            elif v.key < key:
-                p = v
-                v = v.right
-            else:
-                p = v
-                v = v.left
-        return p
-
-    def search(self, key):
-        v = self.findLocation(key)
-        if v and v.key==key:
-            return v
-        else:
-            return None
-
-    def insert(self, key):
-        v = self.forInsert(key)
-        while v != None:
-            if not self.isBalanced(v): # v가 불균형이면
-                z = v
-                if self.bF(z) > 0: # z의 부트리의 높이가 더 큰 쪽을 y로 함.
-                    y = z.left
-                else: y = z.right
-                if self.bF(y) > 0:
-                    x = y.left
-                else: x = y.right
-                v = self.rebalance(z)
-            w = v
-            v = v.parent
-            if v == None:
-                self.root = w
-
-    def forInsert(self, key):
-        p = self.findLocation(key)
-        if p != None and p.key == key:
-            print("The key is already in use.")
-            return
-        else:
-            v = Node(key)
-            if p == None:
-                self.root = v
-            else: # p.key != key
-                v.parent = p
-                if p.key > key:
-                    p.left = v
-                else:
-                    p.right = v
-            self.size += 1
-            v.height = max(self.height(v.left), self.height(v.right)) + 1
-            return v
-
-    def delete(self, u):
-        v = self.deleteByCopying(u) # v는 지워낸 u의 parent
-        while v != None:
-            if not self.isBalanced(v): # v가 불균형이면
-                z = v
-                if self.bF(z) > 0: # z의 부트리의 높이가 더 큰 쪽을 y로 함.
-                    y = z.left
-                else: y = z.right
-                if self.bF(y) > 0:
-                    x = y.left
-                else: x = y.right
-                v = self.rebalance(z)
-            w = v
-            v = v.parent
-            if v == None:
-                self.root = w
-
-    def deleteByCopying(self, x):
-        if x == None: return
-        l, r, p = x.left, x.right, x.parent
-        if l == None:
-            replace = r
-        else:
-            m = l
-            while m.right:
-                m = m.right
-            m.parent.right = m.left
-            if m.left:
-                m.left.parent = m.parent
-            m.right = r
-            m.left = l
-            l.parent = m
-            if r:
-                r.parent = m
-            replace = m            
-        if self.root == x:
-            if replace:
-                replace.parent = None
-            self.root = replace
-        else:
-            if p.left == x:
-                p.left = replace
-            else:
-                p.right = replace
-            if replace:
-                replace.parent = p
-        self.size -= 1
-        if p:
-            p.height = max(self.height(x.left), self.height(x.right)) + 1
-        return p
-
-    def preorder(self, v):
-        if v != None:
-            print(v.key, end=" ")
-            self.preorder(v.left)
-            self.preorder(v.right)
+    def rebalance(self, x, y, z):
+        # rebalance 후 x, y, z 중 맨 위인 z 자리에 오는 노드 return
+        print(f"Rebalance at {z}, {y}, {x}")
+        if z.left == y and y.left == x: # left-left case
+            self.rotateRight(z)
+            return y
+        elif z.right == y and y.right == x:
+            self.rotateLeft(z)
+            return y
+        elif z.left == y and y.right == x: # left-right case
+            self.rotateLeft(y)
+            self.rotateRight(z)
+            return x
+        elif z.right == y and y.left == x:
+            self.rotateRight(y)
+            self.rotateLeft(z)
+            return x
     
-    def inorder(self, v):
-        if v != None:
-            self.inorder(v.left)
-            print(v.key, end=" ")
-            self.inorder(v.right)
+    def isavl(self, v):
+        if v == None: return True
+        return self.isBalanced(v) and self.isavl(v.left) and self.isavl(v.right)
+    
+    def isAVL(self):
+        return self.isavl(self.root)
+    
+    def insert(self, key):
+        v = super(AVLtree, self).insert(key)
+        x, y, z = v, v.parent, None
+        while y:
+            z = y.parent
+            if z and self.isBalanced(z):
+                x, y = y, z
+            else:
+                if z:
+                    self.rebalance(x, y, z)
+                break
+        return v
+    
+    def delete(self, u):
+        v = self.deleteByCopying(u)
+        while v:
+            if not self.isBalanced(v):
+                z = v
+                if z.leftHeight() >= z.rightHeight():
+                    y = z.left
+                else:
+                    y = z.right
+                if y.leftHeight() >= y.rightHeight():
+                    x = y.left
+                else:
+                    x = y.right
+                v = self.rebalance(x, y, z)
+            v= v.parent
 
-    def postorder(self, v):
-        if v != None:
-            self.postorder(v.left)
-            self.postorder(v.right)
-            print(v.key, end=" ")
 
 a = AVLtree()
 a.insert(25)
