8425be438ab9cc77e6eaee474e38ffb27b0c7d72
Jong-eun-Lee <kjayjay40@gmail.com>
2021-02-14 23:44:02 +0900

    2021-02-14

--- Java/D_Operator/src/DA_Operators.java
+++ Java/D_Operator/src/DA_Operators.java
@@ -14,11 +14,16 @@ public class DA_Operators {
 		b = ++a; // a 값 1 증가시킨 것을 b=a 해줌.
 		System.out.println(b); // 12
 		System.out.println(a); // 12
-		int c = -10;
-		c ++;
-		System.out.print(c);
-		-- c;
-		System.out.print(c);
+		// 관계 연산자: ==, !=, >, <, >=, <=
+		// 논리 연산자: &&(AND), ||(OR), !(NOT)
+		System.out.println(1>2 && 3>1);
+		// 비트 연산자: &, |, ^(XOR), ~, <<, >>(비트를 오른쪽으로 시프트)
+		// 비트 연산자는 정수나 문자 등을 2진수로 변환한 후 각 자리의 비트끼리 연산 수행
+		System.out.println(10 & 6); // 1010(2)와 0110(2)를 비트 논리곱해주면 0010(2), 즉 2가 나옴.
+		System.out.println(~5); // 비트 부정 연산자. 1의 보수 반환.
+		System.out.println(Integer.toBinaryString(~5));
+		System.out.println(11 >> 1); // 1011(2) 를 오른쪽으로 한 칸 시프트하여 0101(2), 즉 값 5가 나옴.
+
 	}
 
 }
