0f5e4978be245c079da7f890465f07c8badd0f5b
Jong-eun-Lee <kjayjay40@gmail.com>
2021-02-24 22:46:36 +0900

    2021-02-24

---
+++ Java/F_ForStatement/src/FE_DoWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FE_DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int menu;
+		do
+		{
+			System.out.printf("\nOrder please.\n");
+			System.out.printf("[1] 콜라 | [2] 사이다 | [3] 게토레이 | [4]  ")
+		}
+	}
+
+}
+ 6
